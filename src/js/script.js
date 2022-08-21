1dbdbc8285e9c1eccaeb0470c9cf56c88acf3a58
JL-Wariord-GP <jorge.luis-leones@hotmail.com>
2022-08-21 17:14:43 -0500

    finish

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,8 @@
 const modal = document.querySelector("#formContainer");
 const closeModal = document.querySelector(".close-button");
-
-
 window.onscroll = function(){
-
     scroll = document.documentElement.scrollTop;
-
     header = document.getElementById("header");
-
     if (scroll > 20){
         header.classList.add('nav_mod');
     }else if(scroll < 20){
