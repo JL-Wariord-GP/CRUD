b318c07d82f990334661627a426642e00ebfc350
JL-Wariord-GP <jorge.luis-leones@hotmail.com>
2022-08-21 17:32:16 -0500

    finish

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,6 @@
 const modal = document.querySelector("#formContainer");
 const closeModal = document.querySelector(".close-button");
 
-window.onscroll = function(){
-    scroll = document.documentElement.scrollTop;
-    header = document.getElementById("header");
-    if (scroll > 20){
-        header.classList.add('nav_mod');
-    }else if(scroll < 20){
-        header.classList.remove('nav_mod');
-    }
-}
-
 closeModal.addEventListener("click", () => {
     modal.setAttribute("closing", "");
   
