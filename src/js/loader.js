5098fd95fa7fc3507e69065a359fe924e7836a40
JL-Wariord-GP <jorge.luis-leones@hotmail.com>
2022-08-21 18:48:29 -0500

    end

--- src/js/loader.js
+++ src/js/loader.js
@@ -3,3 +3,13 @@ window.addEventListener('load', function() {
         this.document.getElementById('loader').classList.toggle('loader2')    
     }, 2000);
   })
+
+  window.onscroll = function(){
+    scroll = document.documentElement.scrollTop;
+    header = document.getElementById("header");
+    if (scroll > 20){
+        header.classList.add('nav_mod');
+    }else if(scroll < 20){
+        header.classList.remove('nav_mod');
+    }
+}
